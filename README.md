e9e7b03057170e5d2bd6a63d268ca589597d23c8
Neal Magee <neal.magee@seiu.org>
2013-04-29 13:12:58 -0400

    Tweaked the readme and updated for path in the script.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ aws-iam-certgen
 ===============
 
 A simple bash script that will generate an x.509 key and certificate for you. This allows you to securely use the various AWS API tools [http://aws.amazon.com/developertools]. 
+
+System Requirements
+===================
+- You will need a fairly current version of BASH on your system.
+- OpenSSL is required.
+
+This was written in MacOSX (10.8.3) but works just fine in Ubuntu, Debian, and RedHhat.
--- aws-iam-certgen.sh
+++ aws-iam-certgen.sh
@@ -3,10 +3,13 @@
 echo -e "Enter the name of the user you are creating an IAM key/cert for:"
 read user
 
-cd /Users/nmagee/aws/iam-certs/
+echo -e "Enter the full path to your certificate home [i.e. /home/username/certs/]:"
+read path
 
 echo -e "First I will make a key and then generate a certificate using it. During the cert creation in the next step, you may leave all fields blank. [ENTER]"
 read blah
+
+cd $path
 
 openssl genrsa 1024 > ${user}-key.pem
 openssl req -new -x509 -nodes -sha1 -days 365 -key ${user}-key.pem -outform PEM > ${user}-cert.pem
