3c66bdb017cde6dc37fdcc878f9e1338bb5bc145
Neal Magee <neal.magee@seiu.org>
2013-04-29 13:59:04 -0400

    More elegant whitespace now.

--- aws-iam-certgen.sh
+++ aws-iam-certgen.sh
@@ -1,15 +1,13 @@
 !#/bin/sh
 
 tput clear
-echo -e "Enter a convenient name of the user you are creating an IAM key/cert for [use no spaces]:"
+echo -e "\r\nEnter a convenient name of the user you are creating an IAM key/cert for [use no spaces]:"
 read user
 
-echo -e ""
-echo -e "Enter the full path to your certificate home [i.e. /home/username/certs/]:"
+echo -e "\r\nEnter the full path to your certificate home [i.e. /home/username/certs/]:"
 read path
 
-echo -e ""
-echo -e "First I will make a key and then generate a certificate using it. During the cert creation in the next step, you may leave all fields blank. [ENTER]"
+echo -e "\r\nFirst I will make a key and then generate a certificate using it. During the cert creation in the next step, you may leave all fields blank. [ENTER]"
 read blah
 
 cd $path
@@ -19,11 +17,9 @@ openssl req -new -x509 -nodes -sha1 -days 365 -key ${user}-key.pem -outform PEM 
 openssl pkcs8 -topk8 -in ${user}-key.pem -nocrypt > ${user}-key2.pem
 mv ${user}-key2.pem ${user}-key.pem 
 
-echo -e ""
+echo -e "\r\n"
 echo -e "The ${user} key and certificate have been created. You should now copy the certificate below to paste into your AWS account security credentials:"
-echo -e ""
-echo -e ""
+echo -e "\r\n\r\n"
 cat ${user}-cert.pem
-echo -e ""
-echo -e ""
+echo -e "\r\n\r\n"
 
