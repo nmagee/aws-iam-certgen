7751f70cfa001eecfd46ab51be1fb7d6ef1b45c6
Neal Magee <neal.magee@seiu.org>
2013-04-29 13:08:58 -0400

    Adding the initial script.

---
+++ aws-iam-certgen.sh
@@ -0,0 +1,18 @@
+!#/bin/sh
+
+echo -e "Enter the name of the user you are creating an IAM key/cert for:"
+read user
+
+cd /Users/nmagee/aws/iam-certs/
+
+echo -e "First I will make a key and then generate a certificate using it. During the cert creation in the next step, you may leave all fields blank. [ENTER]"
+read blah
+
+openssl genrsa 1024 > ${user}-key.pem
+openssl req -new -x509 -nodes -sha1 -days 365 -key ${user}-key.pem -outform PEM > ${user}-cert.pem
+openssl pkcs8 -topk8 -in ${user}-key.pem -nocrypt > ${user}-key2.pem
+mv ${user}-key2.pem ${user}-key.pem 
+
+echo -e "The ${user} key and certificate have been created."
+
+
