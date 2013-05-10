e708bd12fb1bcb5493cc6245bc59d760dd7a4d3b
Neal Magee <neal.magee@seiu.org>
2013-05-10 13:35:50 -0400

    Adding some comments and header for script.

--- certgen.sh
+++ certgen.sh
@@ -1,20 +1,23 @@
 !#/bin/sh
+# Created by: Neal Magee <nmagee@gmail.com>
+# Script home: https://github.com/nmagee/aws-x509-certgen
+##############################################################################################################
 
+# Clear the screen and run the user through prompts
 tput clear
-echo -e "\r\nEnter a convenient name of the user you are creating a signing certificate for [use no spaces]:"
+echo -e "\r\nEnter a convenient name of the user you are creating a signing certificate for. [Use no spaces]:"
 read user
 
 echo -e "\r\nHow many days would you like this upload certificate to be valid? [Default is 90]:"
 read age
 
-echo -e "\r\nEnter the full path to your certificate home [i.e. /home/username/.certs/]:"
+echo -e "\r\nEnter the full path to your certificate home [i.e. /home/username/.certs/ - leave blank for current directory. [ENTER]:"
 read path
 
-echo -e "\r\nFirst I will make a key and then generate a certificate using it. During the cert creation in"
-echo -e "the next step, you may leave all fields blank. [ENTER]"
+echo -e "\r\nI will now create a key and then generate a signing certificate using it. [ENTER]"
 read blah
 
-
+# Handle blank PATH values
 if [ -z ${path} ]
 then
   cd ./
@@ -22,11 +25,13 @@ else
   cd ${path}
 fi
 
+# Handle blank AGE values
 if [ -z ${age} ]
 then
   age=90
 fi
 
+# Handle blank USER values
 if [ -z ${user} ]
 then
   user=signingcert
@@ -37,6 +42,7 @@ openssl req -new -x509 -nodes -sha1 -subj "/C=US/ST=Ohio/L=Springfield/O=Dis/CN=
 openssl pkcs8 -topk8 -in ${user}-key.pem -nocrypt > ${user}-key2.pem
 mv ${user}-key2.pem ${user}-key.pem 
 
+# Echo things back to the user
 echo -e "\r\n"
 echo -e "The ${user} key and signing certificate have been created and converted into PEM format. You should now"
 echo -e "copy the certificate below to paste (or upload directly) into your AWS account security credentials:"
